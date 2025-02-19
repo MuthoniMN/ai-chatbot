d6f947ca479e1872b38c455630350375840c0238
Mitch <ndianguimichelle@gmail.com>
2025-02-19 21:32:12 +0300

    feat: adding ai tools types

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,7 +10,8 @@ export type TMessage = {
   id?: number,
   chat_id: number,
   message: string,
-  response: string,
+  language: string,
+  type: 'input' | 'output',
   created_at: Date
 }
 
@@ -20,5 +21,62 @@ export type TTextContext = {
   setChats: Dispatch<SetStateAction<TChat[]>>,
   setChat: Dispatch<SetStateAction<TChat>>,
   messages: TMessage[],
-  setMessages: Dispatch<SetStateAction<TMessage[]>>
+  setMessages: Dispatch<SetStateAction<TMessage[]>>,
+  error: string,
+  setError: Dispatch<SetStateAction<string>>
+}
+
+export type TShortLang = 'es' | 'en' | 'pt' | 'fr' | 'tr' | 'ru';
+
+export type TLang = {
+  [lang in TShortLang | string]: string
+}
+
+export interface AICapabilities {
+  available: 'no' | 'readily' | 'with-download';
+}
+
+export interface AITool {
+  capabilities: () => Promise<AICapabilities>;
+  create: (options?: { 
+  sharedContext?: string,
+  type?: string | 'key-points' | 'tl:dr' | 'teaser' | 'headline',
+  format?: string | 'markdown' | 'plain-text',
+  length?: 'short' | 'medium' | 'long' | string, 
+  sourceLanguage?: string, 
+  targetLanguage?: string, 
+  monitor?: (m: Monitor) => void }) => Promise<LanguageDetectorInstance>;
+}
+
+export interface LanguageDetectorResponse {
+  confidence: number;
+  detectedLanguage: string;
+}
+
+export interface SummarizerOptions {
+  sharedContext?: string,
+  type?: string | 'key-points' | 'tl:dr' | 'teaser' | 'headline',
+  format?: string | 'markdown' | 'plain-text',
+  length?: 'short' | 'medium' | 'long'
+}
+
+export interface LanguageDetectorInstance {
+  ready: Promise<void>;
+  detect: (s: string) => Promise<LanguageDetectorResponse[]>;
+  translate: (s: string) => Promise<string>;
+  summarize: (s: string) => Promise<string>;
+}
+
+export interface Monitor {
+  addEventListener: (event: string, callback: (e: ProgressEvent) => void) => void;
+}
+
+export interface AI {
+  languageDetector?: AITool;
+  translator?: AITool;
+  summarizer?: AITool;
+}
+
+export interface Self {
+  ai?: AI;
 }
