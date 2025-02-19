89c58a4b13ce3b30700447f653277ec678189a45
Mitch <ndianguimichelle@gmail.com>
2025-02-19 21:31:04 +0300

    feat: adding origin trial meta in head

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,22 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router";
 
+const originMeta = document.createElement('meta');
+originMeta.httpEquiv = 'origin-trial';
+originMeta.content = import.meta.env.VITE_SUMMARIZER_TOKEN;
+document.head.append(originMeta);
+
+const orgMeta = document.createElement('meta');
+orgMeta.httpEquiv = 'origin-trial';
+orgMeta.content = import.meta.env.VITE_TRANSLATOR_TOKEN;
+document.head.append(orgMeta);
+
+const oMeta = document.createElement('meta');
+oMeta.httpEquiv = 'origin-trial';
+oMeta.content = import.meta.env.VITE_TRANSLATOR_TOKEN;
+document.head.append(oMeta);
+
+
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <StrictMode>
